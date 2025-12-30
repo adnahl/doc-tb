ba4828fd742fe41452692518246f3a7be0e7468a
aahl <aahl323@outlook.com>
2025-12-30 13:14:16 -0400

    Refactor YouTube component to use iframe directly and enable autoplay

--- components/mdx/youtube.tsx
+++ components/mdx/youtube.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function YouTube({
 	id,
 	title = 'Video',
@@ -6,14 +8,13 @@ export default function YouTube({
 	title?: string
 }) {
 	return (
-		<div>
-			<iframe
-				className="aspect-video w-full"
-				src={`https://www.youtube.com/embed/${id}`}
-				title={title}
-				allowFullScreen
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			/>
-		</div>
+		<iframe
+			width="100%"
+			height="315"
+			src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+			title={title}
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+			allowFullScreen
+		/>
 	)
 }
